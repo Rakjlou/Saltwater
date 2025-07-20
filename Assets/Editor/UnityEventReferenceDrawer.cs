d520092b84705d709ce48c29c024f586e2bb4106
Noé Sierra Velasquez <rakjlou@gmail.com>
2025-07-20 16:21:55 +0200

    Allows to select any event triggered by game object

--- Assets/Editor/UnityEventReferenceDrawer.cs
+++ Assets/Editor/UnityEventReferenceDrawer.cs
@@ -10,62 +10,99 @@ public class UnityEventReferenceDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         EditorGUI.BeginProperty(position, label, property);
-        
+
         // Get the serialized properties
         var targetComponentProp = property.FindPropertyRelative("targetComponent");
         var eventFieldNameProp = property.FindPropertyRelative("eventFieldName");
-        
+
         // Calculate rects
         var totalRect = EditorGUI.PrefixLabel(position, label);
         var componentRect = new Rect(totalRect.x, totalRect.y, totalRect.width, EditorGUIUtility.singleLineHeight);
         var dropdownRect = new Rect(totalRect.x, totalRect.y + EditorGUIUtility.singleLineHeight + 2, totalRect.width, EditorGUIUtility.singleLineHeight);
-        
+
         // Draw component field
         EditorGUI.PropertyField(componentRect, targetComponentProp, GUIContent.none);
-        
-        // Draw dropdown for UnityEvent fields
+
+        // Draw dropdown for UnityEvent fields with component selection
         if (targetComponentProp.objectReferenceValue != null)
         {
             var component = targetComponentProp.objectReferenceValue as Component;
-            var unityEventFields = GetUnityEventFields(component);
-            
-            if (unityEventFields.Length > 0)
+            var gameObject = component.gameObject;
+
+            // Get all components with UnityEvents from the GameObject
+            var componentEventPairs = GetAllComponentEventPairs(gameObject);
+
+            if (componentEventPairs.Count > 0)
             {
-                var fieldNames = unityEventFields.Select(f => f.Name).ToArray();
-                var currentIndex = System.Array.IndexOf(fieldNames, eventFieldNameProp.stringValue);
+                // Create display names: "ComponentName.EventName"
+                var displayNames = componentEventPairs.Select(pair =>
+                    $"{pair.Component.GetType().Name}.{pair.EventField.Name}").ToArray();
+
+                // Find current selection
+                var currentSelection = $"{component.GetType().Name}.{eventFieldNameProp.stringValue}";
+                var currentIndex = System.Array.IndexOf(displayNames, currentSelection);
                 if (currentIndex == -1) currentIndex = 0;
-                
-                var newIndex = EditorGUI.Popup(dropdownRect, "Event Field", currentIndex, fieldNames);
-                if (newIndex >= 0 && newIndex < fieldNames.Length)
+
+                var newIndex = EditorGUI.Popup(dropdownRect, currentIndex, displayNames);
+                if (newIndex >= 0 && newIndex < componentEventPairs.Count)
                 {
-                    eventFieldNameProp.stringValue = fieldNames[newIndex];
+                    var selectedPair = componentEventPairs[newIndex];
+                    targetComponentProp.objectReferenceValue = selectedPair.Component;
+                    eventFieldNameProp.stringValue = selectedPair.EventField.Name;
                 }
             }
             else
             {
-                EditorGUI.LabelField(dropdownRect, "Event Field", "No UnityEvents found");
+                EditorGUI.LabelField(dropdownRect, "No UnityEvents found on GameObject");
             }
         }
         else
         {
-            EditorGUI.LabelField(dropdownRect, "Event Field", "Select a component first");
+            EditorGUI.LabelField(dropdownRect, "Select a component first");
         }
-        
+
         EditorGUI.EndProperty();
     }
-    
+
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         return EditorGUIUtility.singleLineHeight * 2 + 2; // Two lines with small gap
     }
-    
+
     private FieldInfo[] GetUnityEventFields(Component component)
     {
         if (component == null) return new FieldInfo[0];
-        
+
         return component.GetType()
             .GetFields(BindingFlags.Public | BindingFlags.Instance)
             .Where(f => typeof(UnityEventBase).IsAssignableFrom(f.FieldType))
             .ToArray();
+    }
+
+    private System.Collections.Generic.List<ComponentEventPair> GetAllComponentEventPairs(GameObject gameObject)
+    {
+        var pairs = new System.Collections.Generic.List<ComponentEventPair>();
+
+        if (gameObject == null) return pairs;
+
+        var components = gameObject.GetComponents<Component>();
+        foreach (var component in components)
+        {
+            if (component == null) continue;
+
+            var eventFields = GetUnityEventFields(component);
+            foreach (var eventField in eventFields)
+            {
+                pairs.Add(new ComponentEventPair { Component = component, EventField = eventField });
+            }
+        }
+
+        return pairs;
+    }
+
+    private struct ComponentEventPair
+    {
+        public Component Component;
+        public FieldInfo EventField;
     }
 }
